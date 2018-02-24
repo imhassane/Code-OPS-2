3d8334f9c0fc98e439bcdaae9e6a9353bff882b7
imhassane <imthassane@Gmail.com>
2018-02-24 08:22:03 +0100

    Code complet du site
    A cause d'un bug j'ai du reuploader tout le site afin de supprimer la version qui se trouve sur le pc

--- codeops/main/models/UserPart.py
+++ codeops/main/models/UserPart.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .Part import Part
+from .UserCourse import UserCourse
 from django.contrib.auth.models import User
 
 
@@ -7,6 +8,8 @@ class UserPart(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     part = models.ForeignKey(Part, on_delete=models.CASCADE)
+
+    user_course = models.ForeignKey(UserCourse, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return "%s - %s" % (self.user.username, self.course.title)
--- codeops/main/models/UserPath.py
+++ codeops/main/models/UserPath.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .Path import Path
+from .UserCareer import UserCareer
 from django.contrib.auth.models import User
 
 
@@ -7,6 +8,8 @@ class UserPath(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     path = models.ForeignKey(Path, on_delete=models.CASCADE)
+
+    user_career = models.ForeignKey(UserCareer, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return "%s - %s" % (self.user.username, self.path.title)
