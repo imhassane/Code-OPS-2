8268b0ecd7a66661668a55be4e517c71793edbb7
Hassane Sow <imthassane@Gmail.com>
2018-03-09 15:36:41 +0100

    Correction de la redirection lors de la connexion après s'etre déconnecté

--- main/views/forms/logout.py
+++ main/views/forms/logout.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import logout as lg
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import reverse, redirect
 
 
-@login_required
 def logout(request):
-    lg(request)
+    if request.user.is_authenticated:
+        lg(request)
     return redirect(reverse('main:login'))
