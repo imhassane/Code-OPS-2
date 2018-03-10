b4fa108a07a5ca0589e365cc2511a51a789e0d83
Hassane Sow <imthassane@Gmail.com>
2018-03-10 12:33:06 +0100

    Modification de la taille des elelments dans le formulaire de connexion

--- main/forms/login.py
+++ main/forms/login.py
@@ -9,6 +9,6 @@ class LoginForm(forms.Form):
     }))
 
     password = forms.CharField(widget=forms.PasswordInput({
-        'class': 'uk-input uk-form-width-large',
+        'class': 'uk-input uk-width-1-1@s uk-width-1-3@m',
         'placeholder': 'Entez votre mot de passe'
     }))
