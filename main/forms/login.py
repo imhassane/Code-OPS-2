fa35d9a1a696bb9a5a0a08787ca3d3fafe8a3726
Hassane Sow <imthassane@Gmail.com>
2018-03-10 12:54:50 +0100

    Mise a jour de l'interface du site

--- main/forms/login.py
+++ main/forms/login.py
@@ -4,11 +4,11 @@ from django import forms
 class LoginForm(forms.Form):
 
     username = forms.CharField(widget=forms.TextInput({
-        'class': 'uk-input uk-form-width-large',
+        'class': 'uk-input uk-width-large@m uk-width-small@s',
         'placeholder': 'Entre votre nom d\'utilisateur'
     }))
 
     password = forms.CharField(widget=forms.PasswordInput({
-        'class': 'uk-input uk-width-1-1@s uk-width-1-3@m',
+        'class': 'uk-input uk-width-large@m uk-width-small@s',
         'placeholder': 'Entez votre mot de passe'
     }))
--- main/forms/register.py
+++ main/forms/register.py
@@ -3,22 +3,22 @@ from django import forms
 
 class RegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput({
-        'class': 'uk-input uk-form-width-large',
+        'class': 'uk-input uk-width-large@m uk-width-small@s',
         'placeholder': 'Entrez votre nom d\'utilisateur'
     }))
 
     password = forms.CharField(widget=forms.PasswordInput({
-        'class': 'uk-input uk-form-width-large',
+        'class': 'uk-input uk-width-large@m uk-width-small@s',
         'placeholder': 'Entez votre mot de passe'
     }))
 
     repeat = forms.CharField(widget=forms.PasswordInput({
-        'class': 'uk-input uk-form-width-large',
+        'class': 'uk-input uk-width-large@m uk-width-small@s',
         'placeholder': 'Repetez votre mot de passe'
     }))
 
     email = forms.CharField(widget=forms.EmailInput({
-        'class': 'uk-input uk-form-width-large',
+        'class': 'uk-input uk-width-large@m uk-width-small@s',
         'placeholder': 'Entrez votre adresse email'
     }))
 
